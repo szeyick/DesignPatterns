d732e31ef95cf66a509a0470bf053d1255d82bd8
Sze Yick <szeyick@gmail.com>
2015-01-07 22:21:54 +1100

    Initial delivery of Flyweight Pattern
    Delivery of project structure.

---
+++ FlyweightPattern/src/com/designPatterns/flyweightPattern/FlyweightPatternMain.java
@@ -0,0 +1,21 @@
+package com.designPatterns.flyweightPattern;
+
+/**
+ * The {@link FlyweightPatternMain}.
+ * <p>
+ * This class represents the client class
+ * using the flyweight pattern. It is the program
+ * main to demonstraight how the pattern functions.
+ * <p>
+ * @author szeyick
+ */
+public class FlyweightPatternMain {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
