cb141cce98d669299d66e9156e76c7cf49d87eb9
Sze Yick <szeyick@gmail.com>
2015-01-13 20:55:23 +1100

    Initial delivery of Interpreter Pattern
    Delivery -
    
    1. Project Structure for Interpreter Pattern
    2. readMe

---
+++ InterpreterPattern/src/com/designPatterns/interpreterPattern/InterpreterPatternMain.java
@@ -0,0 +1,18 @@
+package com.designPatterns.interpreterPattern;
+
+/**
+ * The {@link InterpreterPatternMain}
+ * <p>
+ * @author szeyick
+ */
+public class InterpreterPatternMain {
+	
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
