c1099d6ea3d8e7c70127b23077eba62e9f84daa5
Sze Yick <szeyick@gmail.com>
2015-01-15 21:58:08 +1100

    Initial delivery of iterator pattern
    Changes -
    
    1. Creation of project structure.
    
    2. Creation of readMe.

---
+++ IteratorPattern/src/com/designPatterns/iteratorPattern/IteratorPatternMain.java
@@ -0,0 +1,21 @@
+package com.designPatterns.iteratorPattern;
+
+/**
+ * The {@link IteratorPatternMain}
+ * <p>
+ * This class represents the main class in the
+ * iterator pattern. 
+ * <p>
+ * @author szeyick
+ */
+public class IteratorPatternMain {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
