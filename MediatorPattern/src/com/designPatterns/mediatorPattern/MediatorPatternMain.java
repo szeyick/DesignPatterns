56aaea2d484f17d61cd5974beb791b9f3ad7855c
Sze Yick <szeyick@gmail.com>
2015-01-17 11:22:01 +1100

    Initial delivery of Mediator Pattern
    Changes -
    
    1. Created Project
    
    2. Created initial readMe.

---
+++ MediatorPattern/src/com/designPatterns/mediatorPattern/MediatorPatternMain.java
@@ -0,0 +1,20 @@
+package com.designPatterns.mediatorPattern;
+
+/**
+ * The {@link MediatorPatternMain}
+ * <p>
+ * This class represents the program main for
+ * the mediator pattern demo.
+ * <p>
+ * @author szeyick
+ */
+public class MediatorPatternMain {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
