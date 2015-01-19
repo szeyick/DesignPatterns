23185b05957966cf29a3a49f4d5518aa9493af3a
Sze Yick <szeyick@gmail.com>
2015-01-19 22:25:08 +1100

    Initial delivery of memento pattern
    Changes -
    
    1. Creation of Memento Pattern project.
    
    2. Creation of readMe with initial notes on the pattern.

---
+++ MementoPattern/src/com/designPatterns/mementoPattern/MementoPatternMain.java
@@ -0,0 +1,20 @@
+package com.designPatterns.mementoPattern;
+
+/**
+ * The {@link MementoPatternMain}
+ * <p>
+ * This is the program main for the memento pattern. 
+ * <p>
+ * @author szeyick
+ */
+public class MementoPatternMain {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguements.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
