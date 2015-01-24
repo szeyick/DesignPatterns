5ef19105b4401ceb15405c2c6f8607303bb39389
Sze Yick <szeyick@gmail.com>
2015-01-25 00:18:01 +1100

    Initial delivery of Template Method
    Changes -
    
    1. readMe
    Added readMe with introduction paragraph to the pattern.
    
    2. Code
    Created project code with main. No implementation as yet.

---
+++ TemplateMethod/src/com/designPatterns/templateMethod/TemplateMethodMain.java
@@ -0,0 +1,20 @@
+package com.designPatterns.templateMethod;
+
+/**
+ * The {@link TemplateMethodMain}
+ * <p>
+ * This class is responsible for demonstrating the template
+ * method.
+ * <p>
+ * @author szeyick
+ */
+public class TemplateMethodMain {
+
+	/**
+	 * The program main.
+	 * @param args - Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
